a3f11f775aa724f709061eec42c93efd4ccf2348
NARAYAN RAI <NARAYANRAI97@GMAIL.COM>
2025-07-18 16:33:45 -0400

    Create TodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
   return(
    <div>
     <h1>My Todos</h1>
+    <TodoForm />
     <TodoList />
    </div>
   )
