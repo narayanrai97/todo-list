0d592f0f18cf5955a091a361631d4f1fbb824a92
NARAYAN RAI <NARAYANRAI97@GMAIL.COM>
2025-07-26 18:02:36 -0400

    Add State for New Todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function App() {
-  const [todos, setTodos] = useState([
-            {id: 1, title: "review resources"},
-            {id: 2, title: "take notes"},
-            {id: 3, title: "code out app"}
-        ]);
+  const initialValue = "Example Text"
+  const todos = [
+                  {id: 1, title: "review resources"},
+                  {id: 2, title: "take notes"},
+                  {id: 3, title: "code out app"},
+                ]
+  const [newTodo, setNewTodo] = useState(initialValue);
 
   return(
    <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{initialValue}</p>
       <TodoList todos={todos}/>
    </div>
   )
