a99cb66831ea54406b076929e60ddca1bfeeb343
NARAYAN RAI <NARAYANRAI97@GMAIL.COM>
2025-07-26 17:44:57 -0400

    Created TodoListItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from  'react';
 import './App.css'
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([
+            {id: 1, title: "review resources"},
+            {id: 2, title: "take notes"},
+            {id: 3, title: "code out app"}
+        ]);
+
   return(
    <div>
-    <h1>My Todos</h1>
-    <TodoForm />
-    <TodoList />
+      <h1>My Todos</h1>
+      <TodoForm />
+      <TodoList todos={todos}/>
    </div>
   )
 }
